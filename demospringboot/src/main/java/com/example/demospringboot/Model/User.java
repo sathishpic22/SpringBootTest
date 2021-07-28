44c59f8236625d59111268339b08778805cf3a8e
dell <dell@MCM-Prod2>
2021-07-28 18:03:57 +0530

    User Entity commit

---
+++ demospringboot/src/main/java/com/example/demospringboot/Model/User.java
@@ -0,0 +1,55 @@
+package com.example.demospringboot.Model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "usertable")
+public class User {
+	
+	@Id
+	@Column(name = "username")
+    private String username;
+
+	@Column(name = "password")
+	private String password;
+
+	@Column(name = "usertype")
+	private String userType;
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getUserType() {
+		return userType;
+	}
+
+	public void setUserType(String userType) {
+		this.userType = userType;
+	}
+
+	public User() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+
+	
+
+}
